b950e7c7e9d57825c327c05f5e785f6b3d1ddf98
qiluge <19525062+qiluge@users.noreply.github.com>
2024-03-17 14:07:35 +0800

    remove some case

--- programs/whirlpool/src/instructions/swap.rs
+++ programs/whirlpool/src/instructions/swap.rs
@@ -1,12 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Token, TokenAccount};
 
-use crate::{
-    errors::ErrorCode,
-    manager::swap_manager::*,
-    state::{TickArray, Whirlpool},
-    util::{to_timestamp_u64, update_and_swap_whirlpool, SwapTickSequence},
-};
+use crate::state::{TickArray, Whirlpool};
 
 #[derive(Accounts)]
 pub struct Swap<'info> {
@@ -40,60 +35,4 @@ pub struct Swap<'info> {
     #[account(seeds = [b"oracle", whirlpool.key().as_ref()],bump)]
     /// CHECK: Oracle is currently unused and will be enabled on subsequent updates
     pub oracle: UncheckedAccount<'info>,
-}
-
-pub fn handler(
-    ctx: Context<Swap>,
-    amount: u64,
-    other_amount_threshold: u64,
-    sqrt_price_limit: u128,
-    amount_specified_is_input: bool,
-    a_to_b: bool, // Zero for one
-) -> Result<()> {
-    let whirlpool = &mut ctx.accounts.whirlpool;
-    let clock = Clock::get()?;
-    // Update the global reward growth which increases as a function of time.
-    let timestamp = to_timestamp_u64(clock.unix_timestamp)?;
-    let mut swap_tick_sequence = SwapTickSequence::new(
-        ctx.accounts.tick_array_0.load_mut().unwrap(),
-        ctx.accounts.tick_array_1.load_mut().ok(),
-        ctx.accounts.tick_array_2.load_mut().ok(),
-    );
-
-    let swap_update = swap(
-        &whirlpool,
-        &mut swap_tick_sequence,
-        amount,
-        sqrt_price_limit,
-        amount_specified_is_input,
-        a_to_b,
-        timestamp,
-    )?;
-
-    if amount_specified_is_input {
-        if (a_to_b && other_amount_threshold > swap_update.amount_b)
-            || (!a_to_b && other_amount_threshold > swap_update.amount_a)
-        {
-            return Err(ErrorCode::AmountOutBelowMinimum.into());
-        }
-    } else {
-        if (a_to_b && other_amount_threshold < swap_update.amount_a)
-            || (!a_to_b && other_amount_threshold < swap_update.amount_b)
-        {
-            return Err(ErrorCode::AmountInAboveMaximum.into());
-        }
-    }
-
-    update_and_swap_whirlpool(
-        whirlpool,
-        &ctx.accounts.token_authority,
-        &ctx.accounts.token_owner_account_a,
-        &ctx.accounts.token_owner_account_b,
-        &ctx.accounts.token_vault_a,
-        &ctx.accounts.token_vault_b,
-        &ctx.accounts.token_program,
-        swap_update,
-        a_to_b,
-        timestamp,
-    )
 }
